43aee64dfeab5f39cb46de6b0b46ef6ba783404c
AndreaGuarracino <andreaguarracino@outlook.com>
2024-04-08 11:00:40 -0500

    manage multiple motifs in input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use bio::io::fasta;
 use regex::Regex;
-use std::{fs::File, io::{self, Write, BufReader, BufWriter}};
+use std::{fs::File, io::{self, Write, BufReader, BufWriter, BufRead}};
 use flate2::read::MultiGzDecoder;
 
 /// Finds specified motifs in a FASTA file and outputs them in BED format with additional details.
@@ -12,9 +12,13 @@ struct Args {
     #[clap(short, long)]
     fasta: String,
 
+    /// File with motifs to search for, one per line
+    #[clap(short = 'f', long)]
+    motif_file: Option<String>,
+
     /// Motif to search for
     #[clap(short, long)]
-    motif: String,
+    motif: Option<String>,
 
     /// Output BED file. Writes to stdout if not specified
     #[clap(short, long)]
@@ -24,12 +28,25 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let fasta_path = &args.fasta;
-    let motif = &args.motif;
-    let reverse_complement = reverse_complement(motif);
+    let mut motifs = Vec::new();
+    // Add the command-line motif if provided
+    if let Some(m) = args.motif {
+        motifs.push(m);
+    }
+    // If a motif file is specified, read motifs from the file and append them to the motifs vector
+    if let Some(ref motif_file) = args.motif_file {
+        let file = File::open(motif_file)?;
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let line = line?;
+            if !line.trim().is_empty() {
+                motifs.push(line);
+            }
+        }
+    }
 
-    let is_gzipped = fasta_path.ends_with(".gz");
-    let reader_box: Box<dyn std::io::Read> = if is_gzipped {
+    let fasta_path = &args.fasta;
+    let reader_box: Box<dyn std::io::Read> = if fasta_path.ends_with(".gz") {
         Box::new(MultiGzDecoder::new(File::open(fasta_path)?))
     } else {
         Box::new(File::open(fasta_path)?)
@@ -41,23 +58,28 @@ fn main() -> io::Result<()> {
         None => Box::new(BufWriter::new(io::stdout())),
     };
 
-    let motif_regex = Regex::new(&regex_pattern(motif)).unwrap();
-    let reverse_regex = Regex::new(&regex_pattern(&reverse_complement)).unwrap();
-
     for result in reader.records() {
         let record = result.expect("Error reading a record");
-        let seq = String::from_utf8(record.seq().to_vec()).unwrap();
 
-        // Search for and write matches of the original motif
-        for mat in motif_regex.find_iter(&seq) {
-            let matched_sequence = &seq[mat.start()..mat.end()];
-            writeln!(writer, "{}\t{}\t{}\t{}\t.\t+\t{}", record.id(), mat.start(), mat.end(), motif, matched_sequence)?;
-        }
+        // Iterate over each motif and its reverse complement for searching
+        for motif in &motifs {
+            let reverse_complement = reverse_complement(&motif);
+            let motif_regex = Regex::new(&regex_pattern(&motif)).unwrap();
+            let reverse_regex = Regex::new(&regex_pattern(&reverse_complement)).unwrap();
 
-        // Search for and write matches of the reverse complement
-        for mat in reverse_regex.find_iter(&seq) {
-            let matched_sequence = &seq[mat.start()..mat.end()];
-            writeln!(writer, "{}\t{}\t{}\t{}\t.\t-\t{}", record.id(), mat.start(), mat.end(), &reverse_complement, matched_sequence)?;
+            let seq = String::from_utf8(record.seq().to_vec()).unwrap();
+
+            // Search for and write matches of the original motif
+            for mat in motif_regex.find_iter(&seq) {
+                let matched_sequence = &seq[mat.start()..mat.end()];
+                writeln!(writer, "{}\t{}\t{}\t{}\t.\t+\t{}", record.id(), mat.start(), mat.end(), motif, matched_sequence)?;
+            }
+
+            // Search for and write matches of the reverse complement
+            for mat in reverse_regex.find_iter(&seq) {
+                let matched_sequence = &seq[mat.start()..mat.end()];
+                writeln!(writer, "{}\t{}\t{}\t{}\t.\t-\t{}", record.id(), mat.start(), mat.end(), &reverse_complement, matched_sequence)?;
+            }
         }
     }
 
